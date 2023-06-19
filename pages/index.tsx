ee7d8e3d7e6eab2184dd503102437e4066fa4953
Federica129 <federica.schillaci@thewavestudio.it>
2023-06-19 10:55:19 +0200

    fix error

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,21 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 
 const LoginPage = (props) => {
   const { user } = props;
+  const router = useRouter();
   console.log(user);
-  if (user) useRouter().push("/home");
-  return <Box>lol</Box>;
+
+  if (user) router.push("/home");
+  return (
+    <Box>
+      <Text as="h1">Valorant</Text>
+      <Box>
+        <Text>Login:</Text>
+        // bottone login google
+      </Box>
+    </Box>
+  );
 };
 
 export default LoginPage;

58faf611897e09a07d17f8573f9a29a4b0fd8b86
Federica129 <federica.schillaci@thewavestudio.it>
2023-08-09 10:21:23 +0200

    new branch

--- src/chakra/index.ts
+++ src/chakra/index.ts
@@ -2,7 +2,6 @@ import { extendTheme } from "@chakra-ui/react";
 import { colors } from "./theme/colors";
 import { breakpoints } from "./theme/breakpoints";
 import HeadingTheme from "./components/Heading";
-// import TextTheme from "./components/Text";
 
 export const theme = extendTheme({
   colors,
